54386cce943e3e99bc636b43ec1a7e67baf7e2ee
Augusto Avelino <avelino.iosdev@gmail.com>
2024-05-15 11:44:45 -0300

    Initial setup

---
+++ LeetCodeSolutions/Application/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  LeetCodeSolutions
+//
+//  Created by Augusto Avelino on 15/05/24.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: ViewController())
+        window?.makeKeyAndVisible()
+    }
+}
+
